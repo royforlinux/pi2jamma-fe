850c3b6c8ebb114197713fad5713ae8c9b007501
timothyehinds <timothyehinds@gmail.com>
2017-11-30 09:57:00 -0800

    start ui crop work.

---
+++ src/ui/Align.cpp
@@ -0,0 +1,21 @@
+#include "ui/Align.hpp"
+
+#if 0
+struct CropSizeResult
+{
+	UnitType 
+};
+
+CropSizeResult cropWidth(Size& targetSize, Size& sourceSize, )
+
+CropRectResult cropRect(
+	const Rect& targetRect,
+	const Rect& sourceRect,
+	VerticalAlignment verticalAlignment,
+	HorizontalAlignment horizontalAlignment,
+	CropMode cropMode)
+{
+
+}
+
+#endif
---
+++ src/ui/Align.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "ui/device/UiDevice.hpp"
+#include "ui/HorizontalAlignment.hpp"
+#include "ui/VerticalAlignment.hpp"
+#include "ui/CropMode.hpp"
+
+namespace ui
+{
+
+#if 0
+struct CropRectResult {
+	Rect& targetRect,
+	Rect& sourceRect
+};
+
+CropRectResult cropRect(
+	const Rect& targetRect,
+	const Rect& sourceRect,
+	VerticalAlignment verticalAlignment,
+	HorizontalAlignment horizontalAlignment,
+	CromMode cropMode );
+#endif
+
+}
+
+
+
+
